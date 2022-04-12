c913cd1ba33c6d72420d983c9cea9b63fafe3a81
AJ Vazquez Alvarez <ajvazquez.teleco@gmail.com>
2022-04-12 23:20:35 +0200

    Fix: fixed bug in exp check

--- cxp/run_jupyter.sh
+++ cxp/run_jupyter.sh
@@ -6,7 +6,7 @@ DEV=""
 #SRC_FOLDER="/home/aj/work/cx_git/CXS338/"
 #DEV=" -v ${SRC_FOLDER}:/opt/cx/venv3/src/cxs338/"
 
-if [[ -z "$EXP" ]; then EXP=`pwd`; fi; EXP=`realpath $EXP`; echo "EXP=$EXP"
+if [[ -z "$EXP" ]]; then EXP=`pwd`; fi; EXP=`realpath $EXP`; echo "EXP=$EXP"
 pushd $EXP > /dev/null
 docker run -p8888:8888 $DEV -v $EXP:/tmp/data -it cxp:latest /opt/cx/run_cxp.sh $@ /opt/cx/venv3/bin/python -m jupyter notebook --no-browser --ip=0.0.0.0
 popd > /dev/null
--- hops/run_x.sh
+++ hops/run_x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ -z "$EXP" ]; then EXP=`pwd`; fi; EXP=`realpath $EXP`; echo "EXP=$EXP"
+if [[ -z "$EXP" ]]; then EXP=`pwd`; fi; EXP=`realpath $EXP`; echo "EXP=$EXP"
 pushd $EXP > /dev/null
 xhost local:root && docker run -v $EXP:/tmp/data -v /tmp/.X11-unix/:/tmp/.X11-unix -e DISPLAY=$DISPLAY -it hops:latest /usr/local/hops/run_ff.sh $@
 popd > /dev/null
